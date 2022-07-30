f4a34438c509bcd76052673b158621d06504a9f3
Alvin Tang <dev@alvinylt.net>
2022-07-30 14:34:29 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# java-misc
+
+This is a repository of some simple Java scripts.
+
+## ShiftCipher
+
+`ShiftCipher` is an implementation of Caesar cipher encryption.
+
+By inputting a shift factor within the range from -26 to 25,
+a string can be encrypted. The code also demonstrates to use of ASCII character
+codes.
+
+The characters are case-sensitive and are always retained. Numbers and
+special characters are not shifted. 
+
+### Example
+
+With the inputs `Hello` and `3`, the output `Khoor` is expected.
+
+@author: Alvin Tang <dev@alvinylt.net>
+
+@last-updated: 2022-07-30
---
+++ src/ShiftCipher/ShiftCipher.java
@@ -0,0 +1,39 @@
+package ShiftCipher;
+
+import java.util.Scanner;
+
+public class ShiftCipher {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str1 = scan.next();
+        Scanner scanNum = new Scanner(System.in);
+        int shiftValue = scanNum.nextInt();
+        char[] ch1 = str1.toCharArray();
+
+        for (int i = 0; i < ch1.length; i++) {
+            int newCharValue = ch1[i] + shiftValue;
+            if (65 <= ch1[i] && ch1[i] <= 90) { // Upper-case letters
+                if (newCharValue > 90) {
+                    ch1[i] = (char) (newCharValue % 90 + 64);
+                } else if (newCharValue < 65) {
+                    ch1[i] = (char) (newCharValue % 65 + 26);
+                } else {
+                    ch1[i] = (char) newCharValue;
+                }
+            } else if (97 <= ch1[i] && ch1[i] <= 122) { // Lower-case letters
+                if (ch1[i] + shiftValue > 122) {
+                    ch1[i] = (char) (newCharValue % 122 + 96);
+                } else if (newCharValue < 97) {
+                    ch1[i] = (char) (newCharValue % 97 + 26);
+                } else {
+                    ch1[i] = (char) newCharValue;
+                }
+            }
+        }
+
+        str1 = String.valueOf(ch1);
+        System.out.println(str1);
+    }
+
+}
